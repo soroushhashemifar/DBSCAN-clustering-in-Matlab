3974c00570ca4066aef34c0a2223fb2654d1a641
Soroush Hashemi Far <soroush.hashemi76@gmail.com>
2018-03-14 01:49:21 +0330

    many bugs fixed, optimized

--- dbscan.m
+++ dbscan.m
@@ -4,7 +4,9 @@ function dataset = dbscan(dataset, epsilon, minPoints)
 	%	2- dataset = [csvfile{2} csvfile{3}];
 	%	3- ds = dbscan(dataset, ..., ...);
 	
-	dataset = [dataset zeros(size(dataset, 1), 1)];
+	dataset = unique(dataset, 'rows'); % usique dataset input matrix w.r.t rows
+	dataset = [dataset zeros(size(dataset, 1), 1)]; % add a column of 1s to first column of the dataset
+
 	clusterID = 1;
 	for i=1:size(dataset, 1)
 		point = dataset(i, :);
@@ -21,9 +23,6 @@ function dataset = dbscan(dataset, epsilon, minPoints)
 
 	% plot dataset and the result of clustering
 	clf;
-	plot(dataset(:, 1), dataset(:, 2), 'ro', 'MarkerSize', 5)
-	pause(5);
-	clf;
 	hold on;
 	plot(dataset(dataset(:, 3)==0, 1), dataset(dataset(:, 3)==0, 2), 'k*', 'MarkerSize', 5)
 	idx = 1;
@@ -31,14 +30,15 @@ function dataset = dbscan(dataset, epsilon, minPoints)
 	for s=1:size(shapes_list, 1)
 		for c=1:size(colors_list, 1)
 			plot(dataset(dataset(:, 3)==idx, 1), dataset(dataset(:, 3)==idx, 2),sprintf('%s',colors_list(c, :),shapes_list(s, :)),'MarkerSize',5)
-			if idx == clusterID
+			idx += 1;
+			if idx >= clusterID
 				flag = true;
 				break
 			end
-			idx += 1;
 		end
 		if flag
 			break
 		end
 	end
+
 endfunction
--- expandCluster.m
+++ expandCluster.m
@@ -1,10 +1,6 @@
 function [flag, dataset] = expandCluster(dataset, point, clusterID, epsilon, minPoints)
-	clusteredPoints = [];
+	
 	seeds = regionQuery(dataset, point, epsilon);
-	%disp('#########point########')	
-	%disp(point)
-	%disp('#########seeds###########')
-	%disp(seeds)
 
 	% if seeds members number is lower than minPoints, all of them are clustered as noise
 	if size(seeds, 1) < minPoints
@@ -13,7 +9,16 @@ function [flag, dataset] = expandCluster(dataset, point, clusterID, epsilon, min
 	end
 
 	seeds(:, 3) = clusterID;
-	dataset(ismember(dataset(:, 1:2), seeds(:, 1:2))(:, 1), 3) = clusterID; % enter clusterIDs to dataset	
+
+	% enter clusterIDs to dataset
+	for i=1:size(seeds, 1)
+		for j=1:size(dataset, 1)
+			if seeds(i, 1:2) == dataset(j, 1:2)
+				dataset(j, 3) = clusterID;
+				break
+			end
+		end
+	end	
 
 	% remove Point itself from seeds
 	x = (seeds(:, 1:2)==point(1:2))(:, 1);
@@ -23,16 +28,21 @@ function [flag, dataset] = expandCluster(dataset, point, clusterID, epsilon, min
 	while size(seeds, 1) != 0
 		currentP = seeds(1, :);
 		result = regionQuery(dataset, currentP, epsilon);
+
 		if size(result, 1) >= minPoints
+			all_zeros_idx = result(:, 3)==0;
+			
+			result(all_zeros_idx, 3) = clusterID;
 			for i=1:size(result, 1)
-
-				if result(i, 3) == 0
-					seeds = [seeds; result(i, :)];
+				for j=1:size(dataset, 1)
+					if result(i, 1:2) == dataset(j, 1:2)
+						dataset(j, 3) = clusterID;
+						break
+					end
 				end
-
-				result(i, 3) = clusterID;
-				dataset(ismember(dataset(:, 1:2), result(i, 1:2))(:, 1), 3) = clusterID;
 			end
+
+			seeds = [seeds; result(all_zeros_idx, :)];
 		end
 		
 		% remove currentP from seeds
@@ -40,5 +50,6 @@ function [flag, dataset] = expandCluster(dataset, point, clusterID, epsilon, min
 		seeds(1, :) = nan;
 		seeds = seeds(~isnan(seeds)(:, 1), :);
 	end
+
 	flag = true;
 endfunction
